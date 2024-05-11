7cab9a6e14884656a208723dda2745cca415d62c
user name <harutyun.ayvazyan.h@gmail.com>
2024-05-11 16:59:50 +0400

    fixed modal call & message user.

--- src/components/pages/singlePage/modalWindowCall/index.js
+++ src/components/pages/singlePage/modalWindowCall/index.js
@@ -1,29 +1,31 @@
+import { useRef } from "react";
 
 import { SlClose } from "react-icons/sl";
 
-import "./style.css"
-import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { toggleModalCallClose } from "../../../../store/featueres/modalCallUser";
 
-const ModalWindowCall = ({ user ,isOpenCall,handleToggleModalCall}) => {
-    const [enter,setEnter] = useState(false)
-    const handleMouseLeave = () => {
-        setEnter(!enter)
-        console.log(enter,"sss")
-    }
+import useClickOutside from "../../../../hooks/useClickOutside";
 
-    const handleMouseEnter =() => {
-        setEnter(!enter)
-        console.log(enter,"aaa")
-    }
-    return (
-        <div className="modalWindowCall" style={isOpenCall ? {display:"flex"}:{display:"none"}} onClick={!enter?handleToggleModalCall:null }>
-            <div className="modalWindowCallContent" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+import "./style.css";
+
+
+const ModalWindowCall = ({ user, isOpenCall }) => {
+    const dispatch = useDispatch();
+    const modalRef = useRef(null);
+
+    const handleClose = () => dispatch(toggleModalCallClose());
+    useClickOutside(modalRef, () => handleClose());
+
+    return isOpenCall && (
+        <div className="modalWindowCall">
+            <div ref={modalRef} className="modalWindowCallContent">
                 <h2>{user.firstName}</h2>
                 <h4>{user.phone}</h4>
-                <SlClose className="modalWindowCallClose" onClick={handleToggleModalCall}/>
+                <SlClose className="modalWindowCallClose" onClick={handleClose} />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ModalWindowCall
+export default ModalWindowCall;
